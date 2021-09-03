aebb1a35e6b63c3141018bdea8ae0e9504744e34
unknown <ga324261@gmail.com>
2021-09-03 11:35:59 +0530

    frame layout

---
+++ app/src/main/java/com/example/revesion/MainActivity.kt
@@ -0,0 +1,16 @@
+package com.example.revesion
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+        //under standing frame layout
+
+
+
+
+    }
+}
